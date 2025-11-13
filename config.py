bab6ff4ee0a64fbf6bfb1008b43d5c2bb70ce5bd
sarchijjbye-lgtm <sarchijjbye@gmail.com>
2025-11-14 02:09:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_CHAT_ID = os.getenv("ADMIN_CHAT_ID")
-SPREADSHEET_NAME = os.getenv("SPREADSHEET_NAME")  # таблица заказов
-GOOGLE_SA_JSON = os.getenv("GOOGLE_SA_JSON")       # ключ сервис-аккаунта
-WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID", 0))
+
+BOT_URL = os.getenv("BOT_URL")
+WEBHOOK_PATH = "/webhook"
+WEBHOOK_URL = f"{BOT_URL}{WEBHOOK_PATH}"
