55b5a6ed5b3e017c2cdc0fe47c28ff268fc6792e
sarchijjbye-lgtm <sarchijjbye@gmail.com>
2025-11-13 22:58:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_CHAT_ID = int(os.getenv("ADMIN_CHAT_ID"))
+SPREADSHEET_NAME = os.getenv("SPREADSHEET_NAME")
+GOOGLE_SA_JSON = os.getenv("GOOGLE_SA_JSON")
